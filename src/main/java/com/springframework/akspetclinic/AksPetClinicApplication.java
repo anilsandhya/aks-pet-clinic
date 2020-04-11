603a9c3775410edb340e860611d7c96d0a8ef113
asharma <anilsandhya@gmail.com>
2020-04-11 12:15:02 -0400

    Initial Project Add

---
+++ src/main/java/com/springframework/akspetclinic/AksPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.akspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AksPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AksPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springframework/akspetclinic/AksPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springframework.akspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class AksPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
